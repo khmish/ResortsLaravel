d3d8d70198b241ebb8fa5f172562de19976e8ce6
khmish <30586743+khmish@users.noreply.github.com>
2021-06-25 00:17:01 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\User;
+use App\Models\Resort;
+use App\Models\Availabletime;
+use App\Models\Rent;
+use App\Models\Feature;
+use App\Models\Featureresort;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +18,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Resort::factory(10)->create();
-        \App\Models\AvailableTime::factory(100)->create();
-        \App\Models\Rent::factory(100)->create();
-        \App\Models\Feature::factory(100)->create();
-        \App\Models\Featureresort::factory(10)->create();
+        User::factory(10)->create();
+        Resort::factory(10)->create();
+        Availabletime::factory(100)->create();
+        Rent::factory(100)->create();
+        Feature::factory(100)->create();
+        Featureresort::factory(10)->create();
     }
 }
